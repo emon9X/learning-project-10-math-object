6b9683e92de70bb7ba0afd224cfb19bcc1f5acca
emon9X <shahariar.emn990@gmail.com>
2024-01-23 11:27:17 +0600

    math operation

---
+++ mathOperation.js
@@ -0,0 +1,48 @@
+//Math.pow(number, exponent/power)
+{
+    console.log(Math.pow(8,2));
+    console.log(Math.pow(2,8));
+}
+
+//Math.sqrt(number)
+
+{
+    console.log(Math.sqrt(36));
+}
+
+//Math.abs(number)
+
+{
+    console.log(Math.abs(-4.5));
+}
+
+//Math.min(numbers)
+{
+    console.log(Math.min(0, 150, 30, 20, -8, -200));
+}
+
+//Math.max(numbers)
+{
+    console.log(Math.max(0, 150, 30, 20, -8, -200));
+}
+
+//Math.log(number)
+//ln not log
+//base is e or Math.E (Eulers number)
+
+{
+    console.log(Math.log(10));
+    console.log(Math.log(Math.E));
+}
+
+//Math.log2(number)
+//base is 2
+{
+    console.log(Math.log2(2));
+}
+
+//Math.log10(number)
+//base is 10
+{
+    console.log(Math.log10(1000));
+}
