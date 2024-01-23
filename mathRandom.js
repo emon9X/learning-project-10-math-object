03e735622024bfa7024121924f355969bccd7bb8
emon9X <shahariar.emn990@gmail.com>
2024-01-23 15:55:31 +0600

    math random

--- mathRandom.js
+++ mathRandom.js
@@ -4,3 +4,29 @@
     console.log(Math.floor(Math.random()));
     console.log(Math.ceil(Math.random()));
 }
+
+{
+    // Returns a random integer from 0 to 9:
+    Math.floor(Math.random() * 10);
+    // Returns a random integer from 0 to 10:
+    Math.floor(Math.random() * 11);
+    // Returns a random integer from 0 to 99:
+    Math.floor(Math.random() * 100);
+    // Returns a random integer from 0 to 100:
+    Math.floor(Math.random() * 101);
+    // Returns a random integer from 1 to 10:
+    Math.floor(Math.random() * 10) + 1;
+    // Returns a random integer from 1 to 100:
+    Math.floor(Math.random() * 100) + 1;
+}
+
+//how to generate random number between 2 numbers
+
+// This JavaScript function always returns a random number between min (included) and max (excluded)
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+//This JavaScript function always returns a random number between min and max (both included):
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
