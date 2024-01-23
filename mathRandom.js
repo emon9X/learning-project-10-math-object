0c19ae100fadb540d49b4bca6ca62b32f02bbd2e
emon9X <shahariar.emn990@gmail.com>
2024-01-23 12:35:41 +0600

    math random

---
+++ mathRandom.js
@@ -0,0 +1,6 @@
+//Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive)
+{
+    console.log(Math.random());
+    console.log(Math.floor(Math.random()));
+    console.log(Math.ceil(Math.random()));
+}
