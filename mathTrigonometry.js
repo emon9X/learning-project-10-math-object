819fb160dec7da639f3bdc033c93de3134ea1f6b
emon9X <shahariar.emn990@gmail.com>
2024-01-23 11:58:44 +0600

    trigonometrical opertions

---
+++ mathTrigonometry.js
@@ -0,0 +1,17 @@
+//Math.sin(abgle in radian)
+//radian to degree
+/**
+ *1 rad = 180/Math.PI
+ * 1 deg = 1 rad * Math.PI/180
+ * 90 deg = 90*Math.PI/180
+ */
+
+ {
+    console.log(Math.sin(90*(Math.PI/180)));
+ }
+
+ //Math.cos(angle in rading)
+
+ {
+    console.log(Math.cos(0 * Math.PI / 180));
+ }
